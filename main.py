0584121d1d71128bfda91418b19b01a620d82a93
irembulut <irembulut_97@hotmail.com>
2020-07-08 23:26:13 +0300

    existing song filter added, it works!!

---
+++ main.py
@@ -0,0 +1,17 @@
+import spotify, secrets, youtube
+from pprint import pprint
+
+
+def main():
+    spofi = spotify.SpotifySongs()
+    playlist_id = spofi.get_or_create_playlist("Youtube Liked Vids")
+
+    youtube_client = youtube.YoutubeVideos("client_secret.json")
+    youtube_songs = youtube_client.get_liked_videos()
+
+    # youtube_songs = [("Bitch Better Have My Money", "Rihanna")]
+    spofi_songs = spofi.add_songs_to_spotify(youtube_songs, playlist_id)
+
+
+if __name__ == "__main__":
+    main()
--- spotify.py
+++ spotify.py
@@ -1,6 +1,7 @@
 import json
 import secrets
 import requests
+from os import getenv
 
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
@@ -40,8 +41,46 @@ class SpotifySongs:
             print("Playlist exists")
             return id
 
-    def get_spotify_uri(self):
-        pass
+    def get_spotify_uri(self, song_name, artist):
+        # Search for the song
+        songname = song_name
+        songartist = artist
 
-    def add_song_to_playlist(self):
-        pass
+        query = f'track:{songname} artist:"{songartist}"'
+        response = self.sp.search(q=query, type="track", limit=5, offset=0)
+        songs = response["tracks"]["items"]
+
+        # only use the first song
+        uri = songs[0]["uri"]
+
+        return uri
+
+    def add_song_to_playlist(self, uri_list, id):
+
+        check_uri_exist = self.sp.playlist_tracks(id, fields="items(track(uri))")
+        existing_uri_song_set = {
+            track["track"]["uri"]
+            for track in check_uri_exist["items"]  # set comprehension
+        }
+
+        # existing_uri_song_list = list()
+        # for track in check_uri_exist["items"]:
+        # existing_uri_song_list.append(track["track"]["uri"])
+
+        for uri in uri_list:
+            if uri not in existing_uri_song_set:
+                self.sp.user_playlist_add_tracks(self.user_id, id, [uri])
+                print(f"added song {uri}")
+            else:
+                print(f"song {uri} already exists")
+
+        print(check_uri_exist)
+        print(existing_uri_song_set)
+
+    def add_songs_to_spotify(self, vids, id):
+        uri_list = [self.get_spotify_uri(name, artist) for name, artist in vids]
+        self.add_song_to_playlist(uri_list, id)
+
+        # uri_list = list()
+        # for name, artist in vids:
+        #     uri_list.append(self.get_spotify_uri(name, artist))
